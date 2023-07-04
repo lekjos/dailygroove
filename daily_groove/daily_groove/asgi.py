d09dacfcdf6e4fbc18889a033b2890e4250ba08c
lekjos <leif@leifkjos.com>
2023-07-04 11:42:32 -0600

    initial

---
+++ daily_groove/daily_groove/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for daily_groove project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daily_groove.settings")
+
+application = get_asgi_application()
