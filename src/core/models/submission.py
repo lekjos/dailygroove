e0619b4fe95f6b47e2bc9660442130ec1fc7c77f
lekjos <leif@leifkjos.com>
2023-07-16 13:10:56 -0700

    feat: update querysets

--- src/core/models/submission.py
+++ src/core/models/submission.py
@@ -2,6 +2,17 @@ from django.conf import settings
 from django.db import models
 
 
+class SubmissionQuerySet(models.QuerySet):
+    def add_submissions_to_game(self, game):
+        from ..models.game_submission import GameSubmission
+
+        submission_pks = self.values_list("pk", flat=True)
+        gs_items = (
+            GameSubmission(submission_id=x, game_id=game.pk) for x in submission_pks
+        )
+        GameSubmission.objects.bulk_create(gs_items, ignore_conflicts=True)
+
+
 class Submission(models.Model):
     class Type(models.IntegerChoices):
         NOT_SPECIFIED = 0
