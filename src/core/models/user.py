e10ed634c3338d48f95ef9edecd07b4849af0bfe
lekjos <leif@leifkjos.com>
2023-07-15 21:13:17 -0700

    feat: user str method

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -57,6 +57,9 @@ class User(AbstractBaseUser, PermissionsMixin):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
+    def __str__(self):
+        return f"{str(self.username)} ({str(self.pk)})"
+
     def delete(self, *args, **kwargs):
         from ..models.player import Player
 
