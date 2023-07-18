d9cf446ccf3d90b9e08db8ac961e76d33359ae01
lekjos <leif@leifkjos.com>
2023-07-17 18:32:41 -0700

    feat: expose static in .env

--- src/daily_groove/settings.py
+++ src/daily_groove/settings.py
@@ -167,7 +167,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = os.getenv("STATIC_URL", "static/")
+STATIC_ROOT = os.getenv("STATIC_ROOT")
+MEDIA_URL = os.getenv("MEDIA_URL", "media/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
