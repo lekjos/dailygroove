bcb4a2691ae473888b8419ce288bab84c53c4d69
lekjos <leif@leifkjos.com>
2023-07-23 12:20:30 -0700

    moar tests

--- src/core/views/tests/test_game_detail.py
+++ src/core/views/tests/test_game_detail.py
@@ -1,11 +1,14 @@
 import random
 
+from django.test import Client
+
 import factory
 import pytest
 
 from core.models.factories import (
     GameFactory,
     PlayerFactory,
+    RoundFactory,
     SubmissionFactory,
     UserFactory,
 )
@@ -28,14 +31,18 @@ class TestGameDetailAnon(ViewTest):
     def game(self, moderator):
         game = GameFactory()
         PlayerFactory(user=moderator, game=game, role=Player.Roles.MODERATOR)
-        players = PlayerFactory.create_batch(5, game=game, role=Player.Roles.PLAYER)
+        players = PlayerFactory.create_batch(10, game=game, role=Player.Roles.PLAYER)
         users = [x.user for x in players]
-        submission_ct = 15
+        submission_ct = 25
         SubmissionFactory.create_batch(
             submission_ct,
             user=factory.Iterator([random.choice(users) for _ in range(submission_ct)]),
         )
         return game
+
+    @pytest.fixture(autouse=True)
+    def initial_round(self, game):
+        return RoundFactory(game=game, winner=None)
 
     @pytest.fixture
     def url_kwargs(self, game):
@@ -50,14 +57,17 @@ class TestRevealAnon(TestGameDetailAnon):
     MAX_QUERIES = None
 
 
-class TestRevealAsMod(TestGameDetailAnon):
+class PostAsMod:
     METHOD = "post"
     EXPECTED_STATUS = 200
-    MAX_QUERIES = 16
 
     @pytest.fixture
     def as_user(self, moderator):
         return moderator
+
+
+class TestRevealAsMod(PostAsMod, TestGameDetailAnon):
+    MAX_QUERIES = 14
 
     @pytest.fixture
     def request_kwargs(self):
@@ -71,3 +81,48 @@ class TestRevealAsMod(TestGameDetailAnon):
     @pytest.mark.django_db
     def test_winner_in_context(self, response):
         assert "submitted_by" in response.context
+
+
+class TestDeclareWinnerAsMod(PostAsMod, TestGameDetailAnon):
+    MAX_QUERIES = 20
+
+    @pytest.fixture
+    def winner(self, game):
+        return Player.objects.filter(game=game).order_by("pk").first()
+
+    @pytest.fixture
+    def request_kwargs(self, winner):
+        """passed into test client"""
+        return {
+            "follow": True,
+            "data": {"action": "Declare Winner", "winner": winner.pk},
+        }
+
+    @pytest.mark.django_db
+    def test_winner_in_context(self, make_request, initial_round, winner):
+        assert initial_round.winner is None
+        response = make_request()
+        assert response.context["current_round"].pk == initial_round.pk
+        actual = response.context["current_round"].winner
+        expected = winner
+        assert actual == expected
+
+
+class TestShuffleAsMod(PostAsMod, TestGameDetailAnon):
+    MAX_QUERIES = 14
+
+    @pytest.fixture
+    def request_kwargs(self):
+        """passed into test client"""
+        return {
+            "follow": True,
+            "data": {"action": "Shuffle"},
+        }
+
+    @pytest.mark.django_db
+    def test_new_submission_in_context(self, make_request, initial_round):
+        response = make_request()
+        assert (
+            initial_round.submission.pk
+            != response.context["current_round"].submission.pk
+        )
