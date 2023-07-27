a49860a0d6065aa8962f07a3b48b3d5033deb66f
lekjos <leif@leifkjos.com>
2023-07-26 19:24:56 -0700

    add game_detail form

--- src/core/forms/game_detail_form.py
+++ src/core/forms/game_detail_form.py
@@ -4,6 +4,15 @@ from core.forms.base_crispy_form import BaseCrispyForm
 from core.models.game import Game
 
 
+class NewGameDetailForm(BaseCrispyForm, forms.ModelForm):
+    def extra_init(self):
+        self.helper.form_tag = False
+
+    class Meta:
+        model = Game
+        fields = ["name", "frequency", "timezone", "round_start_time"]
+
+
 class GameDetailForm(BaseCrispyForm, forms.ModelForm):
     class Meta:
         model = Game
