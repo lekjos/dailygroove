f1a46d124a1f4b35002042f34e6f573fa27fa802
Leif Kjos <leif@leifkjos.com>
2023-10-15 11:16:57 -0700

    Email auth whitelist (#52)

--- src/core/views/sign_up_view.py
+++ src/core/views/sign_up_view.py
@@ -1,15 +1,22 @@
+import logging
+
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME, login
 from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import url_has_allowed_host_and_scheme, urlsafe_base64_encode
 
 from core.utils.utilities import replace_url_params
 
 from ..forms.sign_up_form import SignUpForm
 from ..models import User
 from ..tokens import account_activation_token
+
+logger = logging.getLogger(__name__)
 
 
 def signup_view(request):
@@ -18,6 +25,21 @@ def signup_view(request):
         if form.is_valid():
             user: User = form.save(commit=False)
             user.is_active = False
+            if (
+                user.email.split("@", maxsplit=1)[-1]
+                in settings.WHITELISTED_EMAIL_DOMAINS
+            ):
+                logger.info(
+                    "A user has signed up with a whitelisted domain: %s", user.email
+                )
+                user.email_confirmed = True
+                user.is_active = True
+                user.save()
+                login(request, user)
+                return HttpResponseRedirect(_get_redirect_url(request))
+            logger.info(
+                "A user has signed up with a non-verified domain: %s", user.email
+            )
             user.email_confirmed = False
             user.save()
             current_site = get_current_site(request)
@@ -38,3 +60,16 @@ def signup_view(request):
     else:
         form = SignUpForm()
     return render(request, "signup.html", {"form": form})
+
+
+def _get_redirect_url(request: HttpRequest):
+    """Return the user-originating redirect URL if it's safe."""
+    redirect_to = request.POST.get(
+        REDIRECT_FIELD_NAME, request.GET.get(REDIRECT_FIELD_NAME, "")
+    )
+    url_is_safe = url_has_allowed_host_and_scheme(
+        url=redirect_to,
+        allowed_hosts=request.get_host(),
+        require_https=request.is_secure(),
+    )
+    return redirect_to if url_is_safe else reverse("dashboard")
--- src/core/views/tests/test_sign_up_view.py
+++ src/core/views/tests/test_sign_up_view.py
@@ -19,7 +19,7 @@ class TestSignUpPostInvalid(ViewTest):
     TEMPLATE = "signup.html"
 
 
-class TestSignUpPostValid(ViewTest):
+class TestSignUpPostValidUnknown(ViewTest):
     URL = "signup"
     EXPECTED_STATUS = 302
     METHOD = "post"
@@ -40,3 +40,26 @@ class TestSignUpPostValid(ViewTest):
     @pytest.mark.django_db
     def test_it_redirects_to_correct_page(self, response):
         assert response.url == reverse("account_activation_sent")
+
+
+class TestSignUpPostValidKnown(ViewTest):
+    URL = "signup"
+    EXPECTED_STATUS = 302
+    METHOD = "post"
+    MAX_QUERIES = 11
+
+    @pytest.fixture
+    def request_kwargs(self):
+        """passed into test client"""
+        return {
+            "data": {
+                "username": "testman2",
+                "email": "test@knowndomain.com",
+                "password1": "aStrong1pw;1",
+                "password2": "aStrong1pw;1",
+            },
+        }
+
+    @pytest.mark.django_db
+    def test_it_redirects_to_correct_page(self, response):
+        assert response.url == reverse("dashboard")
