2d9d0b7a0b9ae1f146060cea91a2d208b42d7078
lekjos <leif@leifkjos.com>
2024-03-06 19:24:03 -0800

    test delete upload

--- src/core/views/tests/test_uploads.py
+++ src/core/views/tests/test_uploads.py
@@ -1,6 +1,7 @@
 import pytest
 
 from core.models.factories import UserFactory
+from core.models.submission import Submission
 from testing_utilities.base_view_test import ViewTest
 
 
@@ -19,3 +20,30 @@ class TestUploadAuth(ViewTest):
     @pytest.fixture
     def as_user(self):
         return UserFactory()
+
+
+class TestUploadDelete(ViewTest):
+    URL = "uploads"
+    EXPECTED_STATUS = 200
+    TEMPLATE = "uploads.html"
+    MAX_QUERIES = 11
+    METHOD = "post"
+
+    @pytest.fixture
+    def as_user(self):
+        return UserFactory()
+
+    @pytest.fixture
+    def submission(self, as_user):
+        return Submission.objects.create(
+            user=as_user, title="test", url="http://bing.com"
+        )
+
+    @pytest.fixture
+    def request_kwargs(self, submission):
+        """passed into test client"""
+        return {"follow": True, "data": {"delete-upload": f"{submission.pk}"}}
+
+    @pytest.fixture
+    def response(self, submission, make_request):
+        return make_request()
